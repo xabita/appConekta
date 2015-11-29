982570f8e3bffc2c7d7b2530cc0f2b01f31767cb
xabita <mutskxabaa@gmail.com>
2015-11-29 00:15:31 -0600

    Initial Configuration

--- AppConekta/Start.m
+++ AppConekta/Start.m
@@ -6,13 +6,13 @@
 //  Copyright © 2015 Elizabeth Martínez Gómez. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "Start.h"
 
-@interface ViewController ()
+@interface Start ()
 
 @end
 
-@implementation ViewController
+@implementation Start
 
 - (void)viewDidLoad {
     [super viewDidLoad];
