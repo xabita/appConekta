8231457a086644efcce17cbde14120419bac29da
xabita <mutskxabaa@gmail.com>
2015-11-29 00:47:30 -0600

    Conekta Integration

--- AppConekta/Start.m
+++ AppConekta/Start.m
@@ -7,6 +7,9 @@
 //
 
 #import "Start.h"
+#import "Conekta.h"
+
+
 
 @interface Start ()
 
@@ -15,7 +18,26 @@
 @implementation Start
 
 - (void)viewDidLoad {
+    Conekta *conekta = [[Conekta alloc] init];
+    
+    [conekta setDelegate: self];
+    
+    [conekta setPublicKey:@"key_KJysdbf6PotS2ut2"];
+    
+    [conekta collectDevice];
+    
+    Card *card = [conekta.Card initWithNumber: @"4242424242424242" name: @"Julian Ceballos" cvc: @"123" expMonth: @"10" expYear: @"2018"];
+    
+    Token *token = [conekta.Token initWithCard:card];
+    
+    [token createWithSuccess: ^(NSDictionary *data) {
+        NSLog(@"%@", data);
+    } andError: ^(NSError *error) {
+        NSLog(@"%@", error);
+    }];
+    
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
